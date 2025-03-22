a37f3d9c1d801670fbcc7241a46e7e2ce07af87c
devandresrw <aroldan0@misena.edu.co>
2025-03-21 19:03:14 -0500

    Cloudinary config funcional and cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,30 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', '*.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', '*.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'http://localhost:1337', 'https://1508.agency'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,61 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    documentation: {
+        enabled: true,
+        config: {
+            openapi: '3.0.0',
+            info: {
+                version: '1.0.0',
+                title: 'API - Documentación',
+                description: 'Documentación de tu API',
+                termsOfService: 'https://ejemplo.com/terminos',
+                contact: {
+                    name: 'Equipo API',
+                    email: 'contacto@ejemplo.com',
+                    url: 'https://ejemplo.com',
+                },
+                license: {
+                    name: 'Apache 2.0',
+                    url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
+                },
+            },
+            'x-strapi-config': {
+                path: '/documentation',
+                plugins: [
+                    'email',
+                    'upload',
+                    'users-permissions',
+                    // Añade aquí otros plugins que quieras documentar
+                ],
+            },
+        },
+    },
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_CLOUD_NAME'),
+                api_key: env('CLOUDINARY_API_KEY'),
+                api_secret: env('CLOUDINARY_API_SECRET'),
+                folder: 'agency',
+                use_filename: true,
+                unique_filename: true,
+                overwrite: false,
+                resource_type: 'auto',
+                secure: true, // Asegura que las URLs sean HTTPS
+            },
+            actionOptions: {
+                upload: {
+                    folder: 'agency',
+                    use_filename: true,
+                    unique_filename: true,
+                },
+                uploadStream: {
+                    folder: 'agency',
+                    use_filename: true,
+                    unique_filename: true,
+                },
+                delete: {},
+            },
+        },
+    }
+});
